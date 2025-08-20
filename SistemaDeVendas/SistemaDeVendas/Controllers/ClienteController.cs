20bee63abafd68ba824e67c616a3e04283d9d09a
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-08-20 12:33:09 -0300

    Configurando a view Cliente

--- SistemaDeVendas/SistemaDeVendas/Controllers/ClienteController.cs
+++ SistemaDeVendas/SistemaDeVendas/Controllers/ClienteController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeVendas.Data;
 
 namespace SistemaDeVendas.Controllers
 {
     public class ClienteController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public ClienteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+			var clientes = _context.Cliente.OrderBy(cliente => cliente.Nome).ToList();
+			return View(clientes);
         }
     }
 }
