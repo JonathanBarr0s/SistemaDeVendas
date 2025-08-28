b6ee31f0c191f2a04e33ee951d2c71b7128f8a33
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-08-28 17:52:31 -0300

    Criando a pagina vendedor

---
+++ SistemaDeVendas/SistemaDeVendas/Controllers/VendedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeVendas.Controllers
+{
+	public class VendedorController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- SistemaDeVendas/SistemaDeVendas/Models/VendedorModel.cs
+++ SistemaDeVendas/SistemaDeVendas/Models/VendedorModel.cs
@@ -1,10 +1,16 @@
-﻿namespace SistemaDeVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaDeVendas.Models
 {
-    public class VendedorModel
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-    }
+	public class VendedorModel
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "O campo Nome é obrigatório.")]
+		public string Nome { get; set; }
+		[Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+		[EmailAddress(ErrorMessage = "Informe um e-mail válido.")]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "O campo Senha é obrigatório.")]
+		public string Senha { get; set; }
+	}
 }
