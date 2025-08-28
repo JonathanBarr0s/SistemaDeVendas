be865ef3dd18dccb8404d87531d22cb85d7a9afd
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-08-28 19:00:09 -0300

    Adicionando a pagina Novo Vendedor

--- SistemaDeVendas/SistemaDeVendas/Controllers/VendedorController.cs
+++ SistemaDeVendas/SistemaDeVendas/Controllers/VendedorController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeVendas.Data;
+using SistemaDeVendas.Models;
 
 namespace SistemaDeVendas.Controllers
 {
 	public class VendedorController : Controller
 	{
+		private readonly AppDbContext _context;
+
+		public VendedorController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
 		public IActionResult Index()
 		{
+			var vendedores = _context.Vendedor.OrderBy(v => v.Nome).ToList();
+			return View(vendedores);
+		}
+
+		public IActionResult NovoVendedor()
+		{
 			return View();
+		}
+
+		[HttpPost]
+		public IActionResult RegistrarNovoVendedor(VendedorModel vendedor)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Vendedor.Add(vendedor);
+				_context.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+
+			return View("NovoVendedor", vendedor);
 		}
 	}
 }
