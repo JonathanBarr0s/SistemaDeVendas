b2ad33668a7ab63993fd19e90839d72f3c74d178
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-08-28 21:08:38 -0300

    Configurando a pagina Register

--- SistemaDeVendas/SistemaDeVendas/Controllers/RegisterController.cs
+++ SistemaDeVendas/SistemaDeVendas/Controllers/RegisterController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeVendas.Data;
+using SistemaDeVendas.Models;
 
 namespace SistemaDeVendas.Controllers
 {
 	public class RegisterController : Controller
 	{
+		private readonly AppDbContext _context;
+
+		public RegisterController(AppDbContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult RegistrarNovoVendedor(VendedorModel vendedor)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Vendedor.Add(vendedor);
+				_context.SaveChanges();
+
+				return RedirectToAction("Index", "Login");
+			}
+
+			return View(vendedor);
 		}
 	}
 }
