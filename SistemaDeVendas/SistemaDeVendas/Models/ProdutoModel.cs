1cc021cb93406298987a4854199fdcf705d2d264
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-09-04 16:30:39 -0300

    Criando a tela de Produtos

---
+++ SistemaDeVendas/SistemaDeVendas/Models/ProdutoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaDeVendas.Models
+{
+	public class ProdutoModel
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage ="O campo Nome é obrigatório.")]
+		public string Nome { get; set; }
+		public string Descricao { get; set; }
+		[Required(ErrorMessage ="O campo Preço Unitário é obrigatório.")]
+		public double PrecoUnitario { get; set; }
+		[Required(ErrorMessage ="O campo Quantidade é obrigatório.")]
+		public double QuantidadeEstoque { get; set; }
+		public string UnidadeMedida { get; set; }
+		public string LinkFoto { get; set; }
+	}
+}
