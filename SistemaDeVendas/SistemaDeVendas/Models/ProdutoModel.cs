2dd31b8846b276e4404e4a1726e457534f11b365
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-09-08 18:04:46 -0300

    Configurando a tela de Produtos

--- SistemaDeVendas/SistemaDeVendas/Models/ProdutoModel.cs
+++ SistemaDeVendas/SistemaDeVendas/Models/ProdutoModel.cs
@@ -9,33 +9,16 @@ namespace SistemaDeVendas.Models
 		[Required(ErrorMessage ="O campo Nome é obrigatório.")]
 		public string Nome { get; set; }
 
-		public string Descricao { get; set; }
+		public string? Descricao { get; set; }
 
 		[Required(ErrorMessage = "O campo Preço Unitário é obrigatório.")]
-		public decimal? Preco_Unitario { get; set; }
+		public decimal Preco_Unitario { get; set; }
 
 		[Required(ErrorMessage = "O campo Quantidade é obrigatório.")]
-		public decimal? Quantidade_Estoque { get; set; }
+		public decimal Quantidade_Estoque { get; set; }
 
+		public string? Unidade_Medida { get; set; }
 
-
-
-
-		//public int Id { get; set; }
-
-		//[Required(ErrorMessage = "O campo Nome é obrigatório.")]
-		//public string Nome { get; set; }
-
-		//public string Descricao { get; set; }
-
-		//[Required(ErrorMessage = "O campo Preço Unitário é obrigatório.")]
-		//public decimal? Preco_Unitario { get; set; }
-
-		//[Required(ErrorMessage = "O campo Quantidade é obrigatório.")]
-		//public decimal? Quantidade_Estoque { get; set; }
-
-		//public string Unidade_Medida { get; set; }
-
-		//public string Link_Foto { get; set; }
+		public string? Link_Foto { get; set; }
 	}
 }
