f7202522141c808fa5638c57bb020ea0a5756b69
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-09-12 13:27:01 -0300

    Criando a pagina registrar venda

---
+++ SistemaDeVendas/SistemaDeVendas/Models/VendaModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaDeVendas.Models
+{
+	public class VendaModel
+	{
+		public int Id { get; set; }
+		public DateTime Data { get; set; }
+
+		public decimal Total { get; set; }
+
+		public int Id_Vendedor { get; set; }
+
+		public int Id_Cliente { get; set; }
+	}
+}
