a97bc065d7e72d48470c46f5785ed718c703386a
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-12-21 22:16:43 -0300

    Ajustando o layout da tela de Registro

--- SistemaDeVendas/SistemaDeVendas/Migrations/20251222010601_AddSobrenomeToVendedor.Designer.cs
+++ SistemaDeVendas/SistemaDeVendas/Migrations/20251222010601_AddSobrenomeToVendedor.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SistemaDeVendas.Data;
@@ -11,9 +12,11 @@ using SistemaDeVendas.Data;
 namespace SistemaDeVendas.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251222010601_AddSobrenomeToVendedor")]
+    partial class AddSobrenomeToVendedor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SistemaDeVendas/SistemaDeVendas/Models/VendedorModel.cs
+++ SistemaDeVendas/SistemaDeVendas/Models/VendedorModel.cs
@@ -7,11 +7,17 @@ namespace SistemaDeVendas.Models
 	public class VendedorModel
 	{
 		public int Id { get; set; }
+
 		[Required(ErrorMessage = "O campo Nome é obrigatório.")]
 		public string Nome { get; set; }
+
+		[Required(ErrorMessage = "O campo Nome é obrigatório.")]
+		public string? Sobrenome { get; set; }
+
 		[Required(ErrorMessage = "O campo E-mail é obrigatório.")]
 		[EmailAddress(ErrorMessage = "Informe um e-mail válido.")]
 		public string Email { get; set; }
+
 		[Required(ErrorMessage = "O campo Senha é obrigatório.")]
 		public string Senha { get; set; }
 	}
